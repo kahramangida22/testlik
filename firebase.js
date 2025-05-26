841ecc19a954d6dca79c7fd5004f70ccfaf62d26
kahramangida22 <berkaykah2222@gmail.com>
2025-05-26 23:52:59 +0300

    firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+
+// FIREBASE KENDİ KONSOLUNDAN ALDIĞIN DOĞRU KONFİGİ YAZ!
+const firebaseConfig = {
+  apiKey: "AIzaSyDcneigub2eAJjTrfrkiETuLgy5ule8L6s",
+  authDomain: "testlik.firebaseapp.com",
+  projectId: "testlik",
+  storageBucket: "testlik.appspot.com",
+  messagingSenderId: "668524500496",
+  appId: "1:668524500496:web:579bb4fc5990c87afedc95",
+  measurementId: "G-8ZEYBJCV3T"
+};
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
