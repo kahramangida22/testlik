b4ca473d7a7bc2d450903a0a3588c7a3429a313d
kahramangida22 <berkaykah2222@gmail.com>
2025-06-01 23:25:38 +0300

    Delete lang.js

--- lang.js
+++
@@ -1,6 +0,0 @@
-
-export function detectLanguage() {
-  const lang = navigator.language || navigator.userLanguage;
-  const htmlLang = lang.startsWith('tr') ? 'tr' : 'en';
-  document.documentElement.lang = htmlLang;
-}
