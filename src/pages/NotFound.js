9dcd1597d1606f01529fdd2f8ab2c104299945da
kahramangida22 <berkaykah2222@gmail.com>
2025-07-07 04:25:26 +0300

    Create NotFound.js

---
+++ src/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const NotFound = () => (
+  <div className="container" style={{textAlign: "center", padding: "90px 0"}}>
+    <h1>404</h1>
+    <p>Aradığın sayfa bulunamadı.</p>
+  </div>
+);
+
+export default NotFound;
