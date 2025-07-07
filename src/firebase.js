a1b9c3460f3420c58ec76963fa760cdfe28e2dc7
kahramangida22 <berkaykah2222@gmail.com>
2025-07-07 04:14:55 +0300

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDcneigub2eAJjTrfrkiETuLgy5ule8L6s",
+  authDomain: "testlik.firebaseapp.com",
+  projectId: "testlik",
+  storageBucket: "testlik.firebasestorage.app",
+  messagingSenderId: "668524500496",
+  appId: "1:668524500496:web:579bb4fc5990c87afedc95",
+  measurementId: "G-8ZEYBJCV3T"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
